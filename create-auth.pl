716d24cb60f58d0552f0b77e2ebe4a018f8c506f
Degtyarev Dmitry <degtyarev.dm@gmail.com>
2013-05-08 18:00:44 +0400

    add use base_dir from monitorix.conf

--- create-auth.pl
+++ create-auth.pl
@@ -1,12 +1,15 @@
 #!/usr/bin/perl
 use v5.14;
+use Config::General;
+my $conf = new Config::General("monitorix.conf");
+my %config = $conf->getall;
 say "[create htpasswd]";
 print "username:>";
 my $username = <STDIN>;
 chomp $username;
-system("echo -n $username: > /usr/share/monitorix/htpasswd");
+system("echo -n $username: > $config{base_dir}/htpasswd");
 print "password:>";
 my $password = <STDIN>;
 chomp $password;
-system ("openssl passwd -crypt -salt $username $password >> /usr/share/monitorix/htpasswd");
+system ("openssl passwd -crypt -salt $username $password >> $config{base_dir}/htpasswd");
 say "[htpasswd created]";
