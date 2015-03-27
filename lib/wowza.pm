8678275a5ffa9c58a31bec834aaf81d83996a489
Jordi Sanfeliu <jordi@fibranet.cat>
2015-03-27 12:41:22 +0100

    fixed the text color in the bottom URL of the Wowza graph

--- lib/wowza.pm
+++ lib/wowza.pm
@@ -1073,7 +1073,7 @@ sub wowza_cgi {
 			print "      <td bgcolor='$colors->{title_bg_color}' colspan='2'>\n";
 			print "       <font face='Verdana, sans-serif' color='$colors->{title_fg_color}'>\n";
 			print "       <font size='-1'>\n";
-			print "        <b>&nbsp;&nbsp;<a href='" . $url . "' style='{color: " . $colors->{title_fg_color} . "}'>$url</a><b>\n";
+			print "        <b>&nbsp;&nbsp;<a href='" . $url . "' style='color: " . $colors->{title_fg_color} . "'>$url</a><b>\n";
 			print "       </font></font>\n";
 			print "      </td>\n";
 			print("    </tr>\n");
