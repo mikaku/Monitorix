9527c0233b6408c6ae95572ec9565fa73f6a05fa
Jordi Sanfeliu <jordi@fibranet.cat>
2019-03-13 09:05:34 +0100

    small updates to syncronize nomenclature

--- lib/Monitorix.pm
+++ lib/Monitorix.pm
@@ -157,7 +157,7 @@ sub httpd_setup {
 		}
 	} else {
 		if(!grep {$_ eq $config->{httpd_builtin}->{host}} ("localhost", "127.0.0.1")) {
-			logger("WARNING: the built-in HTTP server has authentication disabled.");
+			logger("WARNING: the HTTP built-in server has authentication disabled.");
 		}
 	}
 
